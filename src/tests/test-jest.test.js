06815d391762b8eeecf986294ab3bdde44a0c478
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2021-10-01 11:37:40 +0200

    add jest at eslint

--- src/tests/test-jest.test.js
+++ src/tests/test-jest.test.js
@@ -3,7 +3,7 @@ const testJest = require("./test-jest")
 describe("Units Tests Jest", () => {
   describe("Test", () => {
     test("adds 1 + 2 to equal 3", () => {
-      expect(testJest(1, 2)).toBe(3)
+      expect(testJest(2, 2)).toBe(3)
     })
   })
 })
