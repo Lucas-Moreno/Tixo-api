e1de5d05cebc7881e577a2b94dbe772ba7d390d2
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2021-10-01 10:46:32 +0200

    add figlet

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
 const helmet = require("helmet")
+const figlet = require("figlet")
 const bodyParser = require("body-parser")
 const process = require("process")
 
@@ -34,9 +35,22 @@ require("./src/routes/routes.js")(app)
 
 // set port, listen for requestsxs
 const PORT = process.env.PORT || 3000
-app.listen(PORT, (err) => {
-  if (err) throw err
-  console.log(`Server is running on port ${PORT}.`)
+app.listen(PORT, () => {
+  figlet(process.env.NAME_APP, {
+    font: "Big Money-ne",
+    horizontalLayout: "full",
+    verticalLayout: "default",
+    width: 180,
+    whitespaceBreak: false
+  }, function (err, data) {
+    if (err) {
+      console.log("Something went wrong...")
+      console.dir(err)
+      return
+    }
+    console.log(data)
+    console.log(`Server is running on port ${PORT}.`)
+  })
 })
 
 module.exports = app
